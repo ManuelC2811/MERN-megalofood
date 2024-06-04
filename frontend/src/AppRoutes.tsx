d30620c1d61482c683964f449d69271cc284ebdd
Manuel Castiblanco <mcastiblancoa@unal.edu.co>
2024-06-04 17:48:14 -0500

    Decimo commit

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -3,7 +3,6 @@ import Layout from "./layouts/layout";
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
-import ProtectedRoute from "./auth/ProtectedRoute";
 import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 import SearchPage from "./pages/SearchPage";
 import DetailPage from "./pages/DetailPage";
@@ -37,32 +36,30 @@ const AppRoutes = () => {
           </Layout>
         }
       />
-      <Route element={<ProtectedRoute />}>
-        <Route
-          path="/order-status"
-          element={
-            <Layout>
-              <OrderStatusPage />
-            </Layout>
-          }
-        />
-        <Route
-          path="/user-profile"
-          element={
-            <Layout>
-              <UserProfilePage />
-            </Layout>
-          }
-        />
-        <Route
-          path="/manage-restaurant"
-          element={
-            <Layout>
-              <ManageRestaurantPage />
-            </Layout>
-          }
-        />
-      </Route>
+      <Route
+        path="/order-status"
+        element={
+          <Layout>
+            <OrderStatusPage />
+          </Layout>
+        }
+      />
+      <Route
+        path="/user-profile"
+        element={
+          <Layout>
+            <UserProfilePage />
+          </Layout>
+        }
+      />
+      <Route
+        path="/manage-restaurant"
+        element={
+          <Layout>
+            <ManageRestaurantPage />
+          </Layout>
+        }
+      />
 
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
