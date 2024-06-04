315cb75f69606edbe127571e02cd3fd9dc7646cb
Manuel Castiblanco <mcastiblancoa@unal.edu.co>
2024-06-04 17:05:54 -0500

    Noveno commit

--- frontend/src/forms/user-profile-form/UserProfileForm.tsx
+++ frontend/src/forms/user-profile-form/UserProfileForm.tsx
@@ -121,20 +121,6 @@ const UserProfileForm = ({
               </FormItem>
             )}
           />
-
-          <FormField
-            control={form.control}
-            name="country"
-            render={({ field }) => (
-              <FormItem className="flex-1">
-                <FormLabel className="font-raleway font-bold">País</FormLabel>
-                <FormControl>
-                  <Input {...field} className="bg-white" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
         </div>
 
         {isLoading ? (
